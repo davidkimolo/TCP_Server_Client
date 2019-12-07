603f69892641d0bb5f9ffadba597467587829652
David <mwikyakimolo@gmail.com>
2019-12-07 14:56:00 +0300

    initial commit

---
+++ src/TCP_Server.cc
@@ -0,0 +1,15 @@
+// include the need libraries and header files 
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+// main program
+int main ()
+{
+    return 0;
+}
