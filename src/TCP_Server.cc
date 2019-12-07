49177eb0f2c11db8f7aeffcf331aed05e08a4375
David <mwikyakimolo@gmail.com>
2019-12-07 15:12:28 +0300

    objectives

--- src/TCP_Server.cc
+++ src/TCP_Server.cc
@@ -8,9 +8,18 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
+// Error return output
+#define Error -1
+
 // main program
 int main ()
 {
-    
+// Create a socket
+// The socket will be binded to an IP/Port
+// Select the socket for listening
+// Accept calls
+// Close the selected listening Port
+// Close socket
+
     return 0;
 }
