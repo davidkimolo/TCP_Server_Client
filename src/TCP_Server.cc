a0d4c5793683ebe608a7b974aeb6f4a0b1e2ed16
David <mwikyakimolo@gmail.com>
2019-12-07 15:48:14 +0300

    binding to IP/Port

--- src/TCP_Server.cc
+++ src/TCP_Server.cc
@@ -22,6 +22,18 @@ if (listening_socket == -1)
     return Error
 }
 // The socket will be binded to an IP/Port
+sockaddr_in new_socket_address;
+new_socket_address.sin_family = AF_INET;
+// convert port to machine correct byte (either little endian or big endian)
+new_socket_address.sin_port = htons(52000);
+// convert an ip address string to a number using this function
+inet_pton(AF_INET, "0.0.0.0", &new_socket_address.sin_addr);
+
+if (bind(listening_socket, (sockaddr*)&new_socket_address, sizeof(new_socket_address)) == -1)
+{
+    std::cerr << "sorry it can't bind to the IP/port" << std::endl;
+    return Error
+}
 // Select the socket for listening
 // Accept calls
 // Close the selected listening Port
