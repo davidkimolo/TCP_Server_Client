ba89e25dcb98ff26465830f81dce937bed6021a6
David <mwikyakimolo@gmail.com>
2019-12-07 15:19:22 +0300

    create a socket

--- src/TCP_Server.cc
+++ src/TCP_Server.cc
@@ -9,12 +9,18 @@
 #include <netdb.h>
 
 // Error return output
-#define Error -1
+#define Error -1;
 
 // main program
 int main ()
 {
 // Create a socket
+int listening_socket = socket(AF_INET, SOCK_STREAM, 0);
+if (listening_socket == -1)
+{
+    std::cerr << "Please connect a socket!" << std::endl;
+    return Error
+}
 // The socket will be binded to an IP/Port
 // Select the socket for listening
 // Accept calls
